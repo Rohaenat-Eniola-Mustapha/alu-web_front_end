e4f91ac2ffd21f0eb5efa5fb0bc86ea4892086e4
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2024-10-29 15:01:16 +0100

    task 2

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+function welcomeMessage(fullName) {
+    alert("Welcome" + " " + fullName)
+}
+
+const guillaume = welcomeMessage("Guilaume");
+const alex = welcomeMessage("Alex");
+const fred = welcomeMessage("Fred");
+
+guillaume();
+alex();
+fred();
